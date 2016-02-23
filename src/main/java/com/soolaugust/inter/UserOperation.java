f09ba4bf318c02c97bfaa45e2983d1712dc05469
soolaugust <soolaugust@gmail.com>
2016-02-23 14:25:48 +0800

    Add update , delete , add Operation 添加更新，删除，新加操作

--- src/main/java/com/soolaugust/inter/UserOperation.java
+++ src/main/java/com/soolaugust/inter/UserOperation.java
@@ -7,4 +7,7 @@ import com.soolaugust.model.User;
 public interface UserOperation {
    public User selectUserByID(int id);
    public List<User> selectUsers(String userName);
+   public void addUser(User user);
+   public void updateUser(User user);
+   public void deleteUser(int id);
 }
