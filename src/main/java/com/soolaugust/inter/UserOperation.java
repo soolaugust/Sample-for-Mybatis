770f79c0738f9516e5b696332c9dabc52d9ba251
soolaugst <soolaugust@gmail.com>
2016-02-23 13:30:09 +0800

    Modify Operation Interface by adding a method 修改接口类，添加一个方法

--- src/main/java/com/soolaugust/inter/UserOperation.java
+++ src/main/java/com/soolaugust/inter/UserOperation.java
@@ -1,7 +1,10 @@
 package com.soolaugust.inter;
 
+import java.util.List;
+
 import com.soolaugust.model.User;
 
 public interface UserOperation {
    public User selectUserByID(int id);
+   public List<User> selectUsers(String userName);
 }
